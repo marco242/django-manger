e62a682848e7fb8e39340b717fa969fa1d4f345a
marco242 <39125731+marco242@users.noreply.github.com>
2018-11-24 12:37:56 +0100

    Update dj.sh

--- dj.sh
+++ dj.sh
@@ -4,16 +4,12 @@
 # Version : 2.0
 # Dependance : +python3.6, +python3.6-venv, -python3-pip (3.6)
 
-
-#Définition de l'interpreteur
-
 #!/bin/bash
 
 # Mes packages
 
 pytz_tar=pytz-2018.4.tar.gz
 django_tar=Django-2.1.2.tar.gz
-#pip_tar=pip-18.1.tar.gz
 
 #Route des PATH (project and venv)
 venv_path=/home/$USER/.venv/
@@ -25,10 +21,7 @@ this="$(dirname "$(realpath "$BASH_SOURCE")")"
 #### Packages 
 django=$this/.packages/$django_tar
 pytz=$this/.packages/$pytz_tar
-#pip=./packages/$pip_tar
 
-
-#Createur de Repos 'Venv et project'
 function create_path {
     if [ ! -d $1 ] && [ ! -d $2 ]; then
        mkdir $1
@@ -37,10 +30,6 @@ function create_path {
        clear
     fi
  }
-
-
-#Fonction de demarage de venv (Si le rep env existe (-d), (-e,-f)pour les fichiers)
-# $1 exprime l'argument
 
  function lance_venv {
     if [ -d $venv_path/$1 ];then
@@ -54,7 +43,6 @@ function create_path {
     fi
 }
 
-#Fonction de creation de venv 
 function cree_venv {
 
     if [[ ! -d $venv_path/$1 ]]; then
@@ -72,8 +60,6 @@ function cree_venv {
     clear
     echo -e "Installation de Django \n"
     pip install  $django
-
-    #Passage de deux arguments ($1:pro.. $2: $1 qui est l'argument du context)
     django_create $project_path $1 
     clear
 
@@ -81,8 +67,6 @@ function cree_venv {
         lance_venv $1
     fi
 }
-
-#Generateur de project 
  function django_create {
     cd  $1 && django-admin startproject $2'_project' 
     cd  $2'_project'
@@ -91,40 +75,22 @@ function cree_venv {
 
  ####### Main #########
 
- #Création des chemin...
 create_path $venv_path $project_path
-#Netoyage de l'Ecran 
 clear
-
-#Demande de choix (Création ou lancement)
 read -p " Hey Marco ! Whats'up ? want you Create or Lanch your project ? (C/L) --> " answer
-
-#Netoyage de l'Ecran 
 clear
-
-
-#Switch d'aprés la réponses (c|C ou l|L)
 case ${answer:0:1} in
     c|C )
-
-        #Demande du nom de la Venv
         read -p "What's a name of your project ? -- > " venv
-
-         #Si la reponse est create|Create (c|C)
         cree_venv $venv
     ;;
     l|L )
-
-        #Demande du nom de la Venv
-        read -p "What's a name of your project ? -- > " venv
-        
-        #Si la reponse est lanch|Lanch (l|L)
+        read -p "Le nom de votre project ? -- > " venv
         lance_venv $venv
     ;;
 
     * )
-    #Si la reponse n'est ni create|Create ou lanch|Lanch
-    clear
+   clear
 	echo -e '\nArgumentError... Call Marco please or  \n(C|c : pour creer le projet)\n(L|l : pour lancer le project)\nBye.... Merci de Relancer'
 
     ;;
